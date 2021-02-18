8ba3861868a76afac72d5c2324e66340b45152b4
srsuti617 <srustigourani617@gmail.com>
2021-02-18 15:18:21 +0000

    max and min

--- Program_5.c
+++ Program_5.c
@@ -5,11 +5,21 @@
 
 int main()
 {
-    int arr[10], n, i;
-    scanf("%d", &n);
+    int arr[5], n, i,max,min;
+    scanf ("%d", &n);
 
     for(i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+      scanf("%d", &arr[i]);
+    max=arr[0];
+    min=arr[0];
+    for(i=0;i<n;i++)
+    {
+      if(arr[i]>max)
+      max=arr[i];
+      if (arr[i]<min)
+      min=arr[i];
+    }
+    printf("Max=%d\nMin=%d",max,min);
 
     //Write your code here
   
